93b3d963f85925df7c19ab4d3d81c3d2f7bab5c9
Yodaman07 <ayaan.irshad@gmail.com>
2023-02-28 20:39:52 -0800

    Started to use websockets for pages, will likely redo this as tab changes involve large reload times for fetching data

--- static/page-control.js
+++ static/page-control.js
@@ -30,10 +30,36 @@ function getNewPage(elmnt) {
         window.location.href = "/"
     } else {
         console.log("Changing tab...")
-        fetch("/data/get_current", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ "icon": filePath, "from": window.location.pathname })
-        }).then(() => window.location.reload())
+        var socket = io()
+        socket.emit("tab-change", { "icon": filePath, "from": window.location.pathname })
+        socket.on('new-tab-data', function (data) {
+            if (window.location.pathname === "/") {
+                update_game_page(data['game_data'], data['themes'], data['current_theme'])
+            }
+            console.log(data)
+        })
+    }
+}
+
+function update_game_page(game_list, theme_list, current_theme) {
+    let current_TList = theme_list[current_theme]
+    console.log("Updating css")
+
+    $('#html').css('background', 'linear-gradient(180deg, ' + current_TList['colors'][0] + ' 0%, ' + current_TList['colors'][1] + ' 100%)');
+    $(".game").css('background', current_TList['colors'][2])
+    $(".settings-background, .game-selection").css('background', current_TList['colors'][3])
+    $(".item-sel").css('background', current_TList['colors'][4])
+
+    $(".game-selection").empty()
+
+    for (const i in theme_list) {
+        if (i === current_theme){
+            let item = $("<div class=\"item-sel\" style=\"background: #80A9F6\"> </div>").append("<img src=" + theme_list[i]['icon'] + " alt=\"Img\" width=\"32px\" height=\"32px\">")
+            $(".game-selection").append(item)
+        }else{
+            let item = $("<div class=\"item-unsel\"> </div>").append("<img src=" + theme_list[i]['icon'] + " alt=\"Img\" width=\"32px\" height=\"32px\">")
+            $(".game-selection").append(item)
+        }
+
     }
 }
