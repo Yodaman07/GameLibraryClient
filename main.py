a7b8f81dfe3b39f3f2adef9cd79417070f1556f9
Yodaman07 <ayaan.irshad@gmail.com>
2023-02-28 20:52:08 -0800

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 002616cb58988da7194bc9e73abda95f6c9dd4c5, reversing
    changes made to 32f10db20effae933c54dfb20a43ecf84dd176f1.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, session, request, escape, url_for, redirect
 from dotenv import load_dotenv
-from flask_socketio import SocketIO
 import platforms
 from flask_socketio import SocketIO
 from userdata import UserData
@@ -21,7 +20,7 @@ socketio = SocketIO(app)
 
 # [BG1, BG2, GameBG, SidebarBG, Sel, Highlight color, Text]
 themes = {
-    "steam": {'colors': ["#6197FF", "#1F407E", "#1E5FDE", "#364561", "#80A9F6", "#FFFFFF"],
+    "steam": {'colors': ["#6197FF", "#1F407E", "#1E5FDE", "#364561", "#80A9F6", "", "#FFFFFF"],
               'icon': '/static/img/steam.svg'},
     "xbox": {'colors': ["#48BD4C", "#18641B", "#2ebf34", "#386D3A", "#82C985", "#FFFFFF"],
              'icon': '/static/img/xbox.svg'},
@@ -58,32 +57,20 @@ def settings():
     return render_template("settings.html", sidebar_items=settings_items, currentItem=session['item'], loggedin=False)
 
 
-@socketio.on('tab-change')
-def change_tab(json):
-    data = {}
-    if not session.get('theme'):
-        session['theme'] = "steam"
-    if not session.get('item'):
-        session['item'] = "profile"
-
-    if session["theme"] == "steam":
-        data = platforms.Steam(steam_id, steam_key).games()
-    elif session["theme"] == "xbox":
-        data = platforms.Xbox(xuid, xbl_key, False).games()
-
-    if json['from'] == "/":
-        for i in themes:
-            if themes[i]['icon'] == json['icon']:
-                session['theme'] = i
-        socketio.emit('new-tab-data', {'game_data': data, 'themes': themes, 'current_theme': session['theme']})
-        print(f"Redirecting to {session['theme']}")
-
-    elif json['from'] == "/settings":
-        for i in settings_items:
-            if settings_items[i]['icon'] == json['icon']:
-                session['item'] = i
-        socketio.emit('new-tab-data', {'sidebar_items': settings_items, 'current_item': session['item'], 'logged_in': False})
-        print(f"Redirecting to {session['item']}")
+@app.route('/data/get_current', methods=["GET", "POST"])
+def currentTheme():
+    if request.method == "POST":
+        if request.json['from'] == "/":
+            for i in themes:
+                if themes[i]['icon'] == request.json['icon']:
+                    session['theme'] = i
+            print(f"Redirecting to {session['theme']}")
+        elif request.json['from'] == "/settings":
+            for i in settings_items:
+                if settings_items[i]['icon'] == request.json['icon']:
+                    session['item'] = i
+            print(f"Redirecting to {session['item']}")
+    return escape(request.json)
 
 
 @app.route('/data/accounts/<action>', methods=['GET', 'POST'])
